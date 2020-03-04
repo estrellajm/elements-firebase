928c89d9a5e8856d6e46cf5111772e3dd0712527
Jose Estrella <jose.estrella@imploie.com>
2020-03-04 03:47:44 -0700

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,43 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, Injector } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
-import { AppComponent } from './app.component';
+import { createCustomElement } from '@angular/elements';
+import { UserPollComponent } from './user-poll/user-poll.component';
+
+const config = {
+  apiKey: "AIzaSyBWRISJCMt9q7tgT1nkNHE4DZXdDWuoE9U",
+  authDomain: "dev-env-1a3a6.firebaseapp.com",
+  databaseURL: "https://dev-env-1a3a6.firebaseio.com",
+  projectId: "dev-env-1a3a6",
+  storageBucket: "dev-env-1a3a6.appspot.com",
+  messagingSenderId: "283816088223",
+  appId: "1:283816088223:web:f5aa656ae5ccaeab"
+}
+import { AngularFireModule } from 'angularfire2';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
 
 @NgModule({
   declarations: [
-    AppComponent
+    UserPollComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(config),
+    AngularFirestoreModule
+  ],
+  entryComponents:[
+    UserPollComponent
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: []
 })
-export class AppModule { }
+export class AppModule { 
+  constructor(private injector: Injector) {}
+
+  ngDoBootstrap() {
+    const el = createCustomElement(UserPollComponent, { injector: this.injector });
+    
+    customElements.define('user-poll', el);
+   }
+}
